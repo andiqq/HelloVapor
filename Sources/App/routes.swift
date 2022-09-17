56d5fa8bb128498fcfdba15d76a7b022f4180a10
Dr. Andreas Plagens <andreas@plagens.eu>
2022-09-17 21:30:49 +0200

    after ch 2 of "Server-Side Swift with Vapor

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,37 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
+    app.get { req in
         "It works!"
     }
 
-    app.get("hello") { req async -> String in
+    app.get("hello") { req -> String in
         "Hello, world!"
     }
+    
+    app.get("hello", ":name") { req -> String in
+        
+        guard let name = req.parameters.get("name")
+        else { throw Abort(.internalServerError) }
+        
+        return "Hello, \(name)!"
+    }
+    
+//    app.post("info") { req throws -> String in
+//        let data = try req.content.decode(InfoData.self)
+//        return "Hello \(data.name)!"
+//    }
+    
+    app.post("info") { req -> InfoResponse in
+        let data = try req.content.decode(InfoData.self)
+        return InfoResponse(request: data)
+    }
+}
+
+struct InfoData: Content {
+    let name: String
+}
+
+struct InfoResponse: Content {
+    let request: InfoData
 }
