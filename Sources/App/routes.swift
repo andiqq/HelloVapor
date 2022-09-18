4f44be05da616fe756c21d001de14702a8725d13
Dr. Andreas Plagens <andreas@plagens.eu>
2022-09-18 17:38:37 +0200

    some  changes (compare with ASP.NET Core)

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,27 +1,13 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req in
-        "It works!"
-    }
+    
+    app.get { req in "It works!" }
 
-    app.get("hello") { req -> String in
-        "Hello, world!"
-    }
+    app.get("hello") { req -> String in "Hello, world!" }
     
-    app.get("hello", ":name") { req -> String in
-        
-        guard let name = req.parameters.get("name")
-        else { throw Abort(.internalServerError) }
-        
-        return "Hello, \(name)!"
-    }
-    
-//    app.post("info") { req throws -> String in
-//        let data = try req.content.decode(InfoData.self)
-//        return "Hello \(data.name)!"
-//    }
-    
+    app.get("hello", ":name") { req -> String in "Hello, \(req.parameters.get("name") ?? "world")!" }
+     
     app.post("info") { req -> InfoResponse in
         let data = try req.content.decode(InfoData.self)
         return InfoResponse(request: data)
@@ -35,3 +21,17 @@ struct InfoData: Content {
 struct InfoResponse: Content {
     let request: InfoData
 }
+
+
+//    app.get("hello", ":name") { req -> String in
+//
+//        guard let name = req.parameters.get("name")
+//        else { throw Abort(.internalServerError) }
+//
+//        return "Hello, \(name)!"
+//    }
+    
+//    app.post("info") { req throws -> String in
+//        let data = try req.content.decode(InfoData.self)
+//        return "Hello \(data.name)!"
+//    }
